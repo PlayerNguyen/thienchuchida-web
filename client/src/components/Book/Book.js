d6539e362c8c069fc8dde9a3fc2ee834ed46c174
PlayerNguyen <daudaua3782@gmail.com>
2021-09-02 14:43:30 +0700

    breadcrumb theme and more color attributes

--- client/src/components/Book/Book.js
+++ client/src/components/Book/Book.js
@@ -2,25 +2,24 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import BookService from "../../services/BookService";
 import Breadcrumb from "../Breadcrumb/Breadcrumb";
+import "./Book.scss"
 
 export default function Book() {
-  
   const [bookInfo, setBookInfo] = useState(null);
   const { slug } = useParams();
 
   useEffect(() => {
-    BookService.getBookBySlug(slug).then(({data}) => {
+    BookService.getBookBySlug(slug).then(({ data }) => {
       // Set a book info
       setBookInfo(data.data[0]);
-    })
+    });
   }, [slug]);
 
   return (
     <div className="book__wrapper container">
-      <Breadcrumb />
       <div className="book">
         <div className="book__header">
-          
+          <Breadcrumb />
         </div>
       </div>
     </div>
--- client/src/components/Breadcrumb/Breadcrumb.js
+++ client/src/components/Breadcrumb/Breadcrumb.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./Breadcrumb.scss";
+import { Link } from "react-router-dom";
 
 export default function Breadcrumb({ data }) {
   return (
     <div className="breadcrumb__wrapper">
       <div className="breadcrumb">
         <div className="breadcrumb__container">
-          <div>Trang chủ</div>
+          <div>
+            <Link className="link link--secondary" to='/'>Trang chủ</Link>
+          </div>
+          <div>
+            <Link className="link link--secondary" to='/truyen'>Trang chủ</Link>
+          </div>
+          <div>
+            <Link className="link link--secondary link--active bold" to='/'>Trang chủ</Link>
+          </div>
+
+          
         </div>
       </div>
     </div>
