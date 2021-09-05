2911895d2d191a29756833a64550961a942ea90b
Zaffran <biprono12@gmail.com>
2021-09-05 12:49:46 +0700

    change some component to reactstrap

--- client/src/components/Admin/AccountManagement/index.js
+++ client/src/components/Admin/AccountManagement/index.js
@@ -3,146 +3,137 @@ import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import ModifyUserModal from "../../Modal/ModifyUser";
 import { v1 } from "uuid";
+import { Button, Table, Row, Col } from "reactstrap";
 
 function AccountManagement() {
-    const sampleData = [
-        {
-            _id: 1,
-            username: "admin",
-            email: "admin@ttcd.gmail.com",
-            admin: true,
-        },
-        {
-            _id: 2,
-            username: "user",
-            email: "user@ttcd.gmail.com",
-            admin: false,
-        },
-    ];
-    const [users, setUsers] = useState(sampleData);
-    const [isModifyModalVisible, setIsModifyModalVisible] = useState(false);
-    const [modifyUser, setModifyUser] = useState(null);
-    // Random key for modify modal, for new instance every render
-    const [randomKey, setRandomKey] = useState(v1());
+  const sampleData = [
+    {
+      _id: 1,
+      username: "admin",
+      email: "admin@ttcd.gmail.com",
+      admin: true,
+    },
+    {
+      _id: 2,
+      username: "user",
+      email: "user@ttcd.gmail.com",
+      admin: false,
+    },
+  ];
+  const [users, setUsers] = useState(sampleData);
+  const [isModifyModalVisible, setIsModifyModalVisible] = useState(false);
+  const [modifyUser, setModifyUser] = useState(null);
+  // Random key for modify modal, for new instance every render
+  const [randomKey, setRandomKey] = useState(v1());
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        // Get users here
-    }, [dispatch]);
+  useEffect(() => {
+    // Get users here
+  }, [dispatch]);
 
-    const handleCloseModifyModal = () => {
-        setModifyUser(null);
-        setIsModifyModalVisible(false);
-    };
+  const handleCloseModifyModal = () => {
+    setModifyUser(null);
+    setIsModifyModalVisible(false);
+  };
 
-    /**
-     *
-     * @param {Object} _modifyUser
-     */
-    const handleOpenModifyModal = (_modifyUser = null) => {
-        setModifyUser(_modifyUser);
-        setIsModifyModalVisible(true);
-        setRandomKey(v1());
-    };
+  /**
+   *
+   * @param {Object} _modifyUser
+   */
+  const handleOpenModifyModal = (_modifyUser = null) => {
+    setModifyUser(_modifyUser);
+    setIsModifyModalVisible(true);
+    setRandomKey(v1());
+  };
 
-    /**
-     *
-     * @param {Object} _newUserInfo
-     */
-    const handleConfirmModifyUser = (_newUserInfo) => {
-        if (!modifyUser) {
-            // Create new user
-        } else {
-            // Modify exist user
-        }
-    };
+  /**
+   *
+   * @param {Object} _newUserInfo
+   */
+  const handleConfirmModifyUser = (_newUserInfo) => {
+    if (!modifyUser) {
+      // Create new user
+    } else {
+      // Modify exist user
+    }
+  };
 
-    const columns = [
-        {
-            title: "Tên đăng nhập",
-            dataIndex: "username",
-            key: "username",
-        },
-        {
-            title: "Email",
-            dataIndex: "email",
-            key: "email",
-        },
-        {
-            title: "Quản trị viên",
-            dataIndex: "admin",
-            key: "admin",
-            render: (admin) => (
-                <>
-                    <input class="form-check-input" type="checkbox" checked={admin} />
-                </>
-            ),
-        },
-        {
-            title: "#",
-            key: "actions",
-            render: (record) => (
-                <>
-                    <button
-                        type="button"
-                        className="btn btn-link"
-                        onClick={() => handleOpenModifyModal(record)}
-                        data-bs-toggle="modal"
-                        data-bs-target="modifyUserModal"
-                    >
-                        Chỉnh sửa
-                    </button>
-                    <button type="button" className="btn btn-link">
-                        Xoá
-                    </button>
-                </>
-            ),
-        },
-    ];
-
-    return (
+  const columns = [
+    {
+      title: "Tên đăng nhập",
+      dataIndex: "username",
+      key: "username",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Quản trị viên",
+      dataIndex: "admin",
+      key: "admin",
+      render: (admin) => (
         <>
-            <div className="row">
-                <div className="col-10 offset-1">Col 4</div>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            {columns.map((_col) => (
-                                <th key={_col.key} scope="col">
-                                    {_col.title}
-                                </th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map((_user, _idx) => (
-                            <tr key={_user._id}>
-                                <td>{_idx + 1}</td>
-                                {columns.map((__col) => (
-                                    <td key={__col.key}>
-                                        {__col.render
-                                            ? __col.render(
-                                                  __col.dataIndex ? _user[__col.dataIndex] : _user
-                                              )
-                                            : _user[__col.dataIndex]}
-                                    </td>
-                                ))}
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-            <ModifyUserModal
-                user={modifyUser}
-                visible={isModifyModalVisible}
-                onCancel={handleCloseModifyModal}
-                onConfirm={handleConfirmModifyUser}
-                key={randomKey}
-            />
+          <input type="checkbox" checked={admin} />
         </>
-    );
+      ),
+    },
+    {
+      title: "#",
+      key: "actions",
+      render: (record) => (
+        <>
+          <Button color="link" onClick={() => handleOpenModifyModal(record)}>
+            Chỉnh sửa
+          </Button>
+          <Button color="link">Xoá</Button>
+        </>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <div className="row">
+        <div className="col-10 offset-1">Col 4</div>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              {columns.map((_col) => (
+                <th key={_col.key} scope="col">
+                  {_col.title}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((_user, _idx) => (
+              <tr key={_user._id}>
+                <td>{_idx + 1}</td>
+                {columns.map((__col) => (
+                  <td key={__col.key}>
+                    {__col.render
+                      ? __col.render(__col.dataIndex ? _user[__col.dataIndex] : _user)
+                      : _user[__col.dataIndex]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+      <ModifyUserModal
+        user={modifyUser}
+        visible={isModifyModalVisible}
+        onCancel={handleCloseModifyModal}
+        onConfirm={handleConfirmModifyUser}
+        key={randomKey}
+      />
+    </>
+  );
 }
 
 export default AccountManagement;
