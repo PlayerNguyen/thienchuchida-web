d7ac9f817ce2acb99c3762745f9456c26532eacd
Zaffran <biprono12@gmail.com>
2021-09-07 22:31:33 +0700

    update account management on admin side

--- client/src/components/Admin/AccountManagement/index.js
+++ client/src/components/Admin/AccountManagement/index.js
@@ -3,35 +3,35 @@ import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import ModifyUserModal from "../../Modal/ModifyUser";
 import { v1 } from "uuid";
-import { Button, Table, Row, Col } from "reactstrap";
+import { Button, Table} from "react-bootstrap";
 import UserService from "../../../services/UserService";
 
 function AccountManagement() {
-    const [users, setUsers] = useState([]);
-    const [isModifyModalVisible, setIsModifyModalVisible] = useState(false);
-    const [modifyUser, setModifyUser] = useState(null);
-    // Random key for modify modal, for new instance every render
-    const [randomKey, setRandomKey] = useState(v1());
+  const [users, setUsers] = useState([]);
+  const [isModifyModalVisible, setIsModifyModalVisible] = useState(false);
+  const [modifyUser, setModifyUser] = useState(null);
+  // Random key for modify modal, for new instance every render
+  const [randomKey, setRandomKey] = useState(v1());
 
   const dispatch = useDispatch();
 
-    useEffect(() => {
-        getAllUser();
-    }, [dispatch]);
+  useEffect(() => {
+    getAllUser();
+  }, [dispatch]);
 
-    const getAllUser = async () => {
-        try {
-            const resp = await UserService.getAllUsers();
-            setUsers(resp.data.data);
-        } catch (e) {
-            console.error(e);
-        }
-    };
+  const getAllUser = async () => {
+    try {
+      const resp = await UserService.getAllUsers();
+      setUsers(resp.data.data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-    const handleCloseModifyModal = () => {
-        setModifyUser(null);
-        setIsModifyModalVisible(false);
-    };
+  const handleCloseModifyModal = () => {
+    setModifyUser(null);
+    setIsModifyModalVisible(false);
+  };
 
   /**
    *
@@ -55,83 +55,77 @@ function AccountManagement() {
     }
   };
 
-    const columns = [
-        {
-            title: "Tên đăng nhập",
-            dataIndex: "username",
-            key: "username",
-        },
-        {
-            title: "Email",
-            dataIndex: "email",
-            key: "email",
-        },
-        {
-            title: "Quản trị viên",
-            dataIndex: "admin",
-            key: "admin",
-            render: (admin) => (
-                <>
-                    <input type="checkbox" readOnly checked={admin} />
-                </>
-            ),
-        },
-        {
-            title: "#",
-            key: "actions",
-            render: (record) => (
-                <>
-                    <Button color="link" onClick={() => handleOpenModifyModal(record)}>
-                        Chỉnh sửa
-                    </Button>
-                    <Button color="link">Xoá</Button>
-                </>
-            ),
-        },
-    ];
-
-    return (
+  const columns = [
+    {
+      title: "Tên đăng nhập",
+      dataIndex: "username",
+      key: "username",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Quản trị viên",
+      dataIndex: "admin",
+      key: "admin",
+      render: (admin) => <input type="checkbox" readOnly checked={admin} />,
+    },
+    {
+      title: "#",
+      key: "actions",
+      render: (record) => (
         <>
-            <div className="row">
-                <div className="col-10 offset-1">Col 4</div>
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            {columns.map((_col) => (
-                                <th key={_col.key} scope="col">
-                                    {_col.title}
-                                </th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map((_user, _idx) => (
-                            <tr key={_user._id}>
-                                <td>{_idx + 1}</td>
-                                {columns.map((__col) => (
-                                    <td key={__col.key}>
-                                        {__col.render
-                                            ? __col.render(
-                                                  __col.dataIndex ? _user[__col.dataIndex] : _user
-                                              )
-                                            : _user[__col.dataIndex]}
-                                    </td>
-                                ))}
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
-            </div>
-            <ModifyUserModal
-                user={modifyUser}
-                visible={isModifyModalVisible}
-                onCancel={handleCloseModifyModal}
-                onConfirm={handleConfirmModifyUser}
-                key={randomKey}
-            />
+          <Button variant="link" onClick={() => handleOpenModifyModal(record)}>
+            Chỉnh sửa
+          </Button>
+          <Button variant="link">Xoá</Button>
         </>
       ),
+    },
+  ];
+
+  return (
+    <>
+      <Button variant="link" onClick={() => handleOpenModifyModal()}>
+        Thêm tài khoản mới
+      </Button>
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            {columns.map((_col) => (
+              <th key={_col.key} scope="col">
+                {_col.title}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((_user, _idx) => (
+            <tr key={_user._id}>
+              <td>{_idx + 1}</td>
+              {columns.map((__col) => (
+                <td key={__col.key}>
+                  {__col.render
+                    ? __col.render(__col.dataIndex ? _user[__col.dataIndex] : _user)
+                    : _user[__col.dataIndex]}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <ModifyUserModal
+        user={modifyUser}
+        visible={isModifyModalVisible}
+        onClose={handleCloseModifyModal}
+        onConfirm={handleConfirmModifyUser}
+        key={randomKey}
+      />
+    </>
+  );
 }
 
 export default AccountManagement;
--- client/src/components/Modal/ModifyUser.js
+++ client/src/components/Modal/ModifyUser.js
@@ -1,24 +1,200 @@
-import React from "react";
-// import { Modal, Form, Input, Checkbox } from "antd";
+import React, { useEffect, useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+import { toast } from "react-toastify";
 
-function ModifyUserModal({ visible, onConfirm, onCancel, user = null }) {
-    const initUserInfo = {
-        username: "",
-        email: "",
-        admin: false,
-    };
+function ModifyUserModal({ visible, onConfirm, onClose, user = null }) {
+  const [userInfo, setUserInfo] = useState({
+    username: "",
+    email: "",
+    admin: false,
+    changingPassword: false,
+    currentPassword: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [validator, setValidator] = useState({
+    passwordMatch: true,
+  });
+  // state of interacting with inputs
+  const [inputState, setInputState] = useState({
+    focusPassword: false,
+  });
 
-    // const [form] = Form.useForm();
+  useEffect(() => {
+    if (user) {
+      // override init state with existing properties
+      setUserInfo({ ...userInfo, ...user });
+    }
+  }, [user]);
 
-    const handleConfirmModifyUser = () => {
-        // const newUserInfo = form.getFieldsValue(true);
-        // onConfirm(newUserInfo);
-    };
+  /**
+   * 
+   * @returns {Boolean}
+   */
+  const handleCheckAllValidatorsValid = () => {
+    let isInputValid = true;
+    Object.entries(validator).forEach((_key, _value) => {
+      if (!_value) {
+        isInputValid = false;
+      }
+    });
+    return isInputValid;
+  };
 
-    return (
-        <>
-        </>
-    );
+  const handleConfirmModifyUser = () => {
+    const isInputValid = handleCheckAllValidatorsValid();
+    if (isInputValid) {
+      onConfirm(userInfo);
+    } else {
+      toast.error("Dữ liệu không hợp lệ! Vui lòng kiểm tra lại.");
+    }
+  };
+
+  /**
+   *
+   * @param {String} property
+   */
+  const handleChangeUserInfo = (property) => (e) => {
+    const { value } = e.target;
+    setUserInfo({ ...userInfo, [property]: value });
+  };
+
+  /**
+   *
+   * @param {HtmlEvent} e
+   */
+  const handleChangeUserRole = (e) => {
+    const { checked } = e.target;
+    setUserInfo({ ...userInfo, admin: checked });
+  };
+
+  const handleAllowChangePassword = () => {
+    setUserInfo({
+      ...userInfo,
+      changingPassword: !userInfo.changingPassword,
+      password: "",
+      confirmPassword: "",
+    });
+  };
+
+  /**
+   *
+   * @param {Boolean} focusState
+   */
+  const handleFocusOnPassword = (focusState) => {
+    setInputState({ ...inputState, focusPassword: focusState });
+    // lost focus
+    if (!focusState) {
+      const { password, confirmPassword } = userInfo;
+      let passwordMatch = true;
+      if ((password || confirmPassword) && password !== confirmPassword) {
+        passwordMatch = false;
+      }
+      setValidator({
+        ...validator,
+        passwordMatch,
+      });
+    }
+  };
+
+  return (
+    <>
+      <Modal show={visible} onHide={onClose}>
+        <Modal.Header closeButton>{user ? "Cập nhật tài khoản" : "Tạo tài khoản mới"}</Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                value={userInfo.username}
+                name="username"
+                placeholder="Nhập tên đăng nhập"
+                onChange={handleChangeUserInfo("username")}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                value={userInfo.email}
+                name="email"
+                type="email"
+                placeholder="Nhập email"
+                onChange={handleChangeUserInfo("email")}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Check
+                checked={userInfo.admin}
+                name="admin"
+                type="checkbox"
+                label="Quản trị viên"
+                onChange={handleChangeUserRole}
+              />
+            </Form.Group>
+            {user && (
+              <Form.Group className="mb-3">
+                <Button variant="link" onClick={handleAllowChangePassword}>
+                  Đổi mật khẩu
+                </Button>
+              </Form.Group>
+            )}
+            {(!user || userInfo.changingPassword) && (
+              <>
+                {userInfo.changingPassword && (
+                  <Form.Group className="mb-3">
+                    <Form.Label>Mật khẩu hiện tại</Form.Label>
+                    <Form.Control
+                      value={userInfo.currentPassword}
+                      name="currentPassword"
+                      type="password"
+                      onChange={handleChangeUserInfo("currentPassword")}
+                    />
+                  </Form.Group>
+                )}
+                <Form.Group className="mb-3">
+                  <Form.Label>Mật khẩu {userInfo.changingPassword && "mới"}</Form.Label>
+                  <Form.Control
+                    value={userInfo.password}
+                    name="password"
+                    type="password"
+                    onFocus={() => handleFocusOnPassword(true)}
+                    onBlur={() => handleFocusOnPassword(false)}
+                    onChange={handleChangeUserInfo("password")}
+                  />
+                  {!inputState.focusPassword &&
+                    !validator.passwordMatch &&
+                    (userInfo.password || userInfo.confirmPassword) && (
+                      <Form.Text className="text-bold text-danger">
+                        Mật khẩu không trùng khớp!
+                      </Form.Text>
+                    )}
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Xác nhận mật khẩu</Form.Label>
+                  <Form.Control
+                    value={userInfo.confirmPassword}
+                    name="confirmPassword"
+                    type="password"
+                    onFocus={() => handleFocusOnPassword(true)}
+                    onBlur={() => handleFocusOnPassword(false)}
+                    onChange={handleChangeUserInfo("confirmPassword")}
+                  />
+                </Form.Group>
+              </>
+            )}
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleConfirmModifyUser}>
+            Xác nhận
+          </Button>
+          <Button variant="secondary" onClick={onClose}>
+            Đóng
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
 export default ModifyUserModal;
