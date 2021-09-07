1fb44bc1ebe1f437bcca49645d577898cbfc94cf
Zaffran <biprono12@gmail.com>
2021-09-07 19:32:31 +0700

    fix wrong property name

--- client/src/components/Admin/AdminNavbar/index.js
+++ client/src/components/Admin/AdminNavbar/index.js
@@ -4,12 +4,14 @@ import { Link } from "react-router-dom";
 function AdminNavbar() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-dark">
-        <Link className="navbar-brand admin__header__brand">Thiên Chu Chi Dạ</Link>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <Link class="nav-link" to="/">
+      <nav className="navbar navbar-expand-lg navbar-dark">
+        <Link className="navbar-brand admin__header__brand" to="/">
+          Thiên Chu Chi Dạ
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">
                 Trang chủ
               </Link>
             </li>
