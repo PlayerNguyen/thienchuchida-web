39802f0ae13772b7aedbe205270ee6f4a7feadf0
Zaffran <biprono12@gmail.com>
2021-09-12 16:41:51 +0700

    add confirm modal

---
+++ client/src/components/Modal/Confirm.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button, Modal } from "react-bootstrap";
+
+function ConfirmModal({ visible, onConfirm, onClose, title, content }) {
+  return (
+    <>
+      <Modal show={visible} onHide={onClose}>
+        <Modal.Header closeButton>{title}</Modal.Header>
+        <Modal.Body>{content}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={onConfirm}>
+            Xác nhận
+          </Button>
+          <Button variant="secondary" onClick={onClose}>
+            Đóng
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default ConfirmModal;
