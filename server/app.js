40dced69c990b1a49d4710b676951dfdebd833fe
PlayerNguyen <daudaua3782@gmail.com>
2021-09-23 17:18:03 +0700

    separated origin config to env

--- server/app.js
+++ server/app.js
@@ -23,8 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    // origin: MiscConfig.cors.whitelist,
-    origin: "http://localhost:3000",
+    origin: process.env.ORIGIN_URL,
     credentials: true,
   })
 );
