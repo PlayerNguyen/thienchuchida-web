42451c76f954204ba28a49646418fe7bca01b849
Peanutzz2011 <ititiu20261@student.hcmiu.edu.vn>
2021-10-20 12:37:13 +0700

    test for not using loading case

--- client/src/components/Admin/ResourceManager/ResourceImage.js
+++ client/src/components/Admin/ResourceManager/ResourceImage.js
@@ -3,62 +3,68 @@ import axiosInstance from "../../../helpers/axiosInstance";
 import imageHelper from "../../../helpers/imageHelper";
 import Loading from "../../Loading/Loading";
 import "./ResourceItem.scss";
-import ServerConfig from '../../../config/server.config'
+import ServerConfig from "../../../config/server.config";
 
 export default function ResourceImage({ id, alt, height, ...children }) {
-  const [contentType, setContentType] = useState(null);
+  // const [contentType, setContentType] = useState(null);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    if (id) {
-      // Set loading an image to true
-      setLoading(true);
+  // useEffect(() => {
+  //   if (id) {
+  //     // Set loading an image to true
+  //     setLoading(false);
 
-      // Then load it
-      axiosInstance
-        .get(imageHelper.getBase64ResourceUrl(id))
-        .then((response) => {
-          const { headers } = response;
-          setContentType(headers["content-type"]);
-          setData(response.data);
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-    }
+  //     // Then load it
+  //     axiosInstance
+  //       .get(imageHelper.getBase64ResourceUrl(id))
+  //       .then((response) => {
+  //         const { headers } = response;
+  //         setContentType(headers["content-type"]);
+  //         setData(response.data);
+  //       })
+  //       .finally(() => {
+  //         setLoading(false);
+  //       });
+  //   }
 
-    // Clean up the component
-    return () => {
-      setLoading(false)
-      setContentType(null)
-      setLoading(false)
-    };
-  }, [id]);
+  //   // Clean up the component
+  //   return () => {
+  //     setLoading(false)
+  //     setContentType(null)
+  //     setLoading(false)
+  //   };
+  // }, [id]);
 
   return (
-    <div className="w-100" {...children}>
-      {loading ? (
-        <Loading />
-      ) : id ? (
-        data ? (
-          <img
-            src={`data:${contentType};base64, ${data}`}
-            alt={alt || ``}
-            className="w-100 img--resource"
-            style={{ maxHeight: height }}
-          />
-        ) : (
-          <div className="text-danger text-center p-5">Ảnh không hợp lệ.</div>
-        )
-      ) : (
-        <img
-            src={ServerConfig.DEFAULT_THUMBNAIL}
-            alt={alt || ``}
-            className="w-100 img--resource"
-            style={{ maxHeight: height }}
-          />
-      )}
-    </div>
+    // <div className="w-100" {...children}>
+    //   {loading ? (
+    //     <Loading />
+    //   ) : id ? (
+    //     data ? (
+    //       <img
+    //         src={`data:${imageHelper.getBase64ResourceUrl(id)};base64, ${imageHelper.getBase64ResourceUrl(id)}`}
+    //         alt={alt || ``}
+    //         className="w-100 img--resource"
+    //         style={{ maxHeight: height }}
+    //       />
+    //     ) : (
+    //       <div className="text-danger text-center p-5">Ảnh không hợp lệ.</div>
+    //     )
+    //   ) : (
+    //     <img
+    //         src={ServerConfig.DEFAULT_THUMBNAIL}
+    //         alt={alt || ``}
+    //         className="w-100 img--resource"
+    //         style={{ maxHeight: height }}
+    //       />
+    //   )}
+    // </div>
+    <img
+      src={imageHelper.getRawResourceUrl(id)}
+      alt={alt || ``}
+      className="w-100 img--resource"
+      style={{ maxHeight: height }}
+    />
   );
 }
