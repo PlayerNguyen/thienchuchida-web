53ac072f492996705b5bdf11461ebac2eed50899
Peanutzz2011 <ititiu20261@student.hcmiu.edu.vn>
2021-10-20 16:20:51 +0700

    update pm2 configuration

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,23 +1,14 @@
 module.exports = {
-  apps : [{
-    name: "",
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+  apps: [
+    {
+      name: "tccd-server",
+      script: "./www",
+      env_production: {
+        NODE_ENV: "production",
+      },
+      env_development: {
+        NODE_ENV: "development",
+      },
+    },
+  ],
 };
