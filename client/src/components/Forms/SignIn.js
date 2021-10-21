b562548d2dc4dc099ee1a8aa88b8390285091927
Peanutzz2011 <ititiu20261@student.hcmiu.edu.vn>
2021-10-21 12:56:31 +0700

    remove popup sign in and optimize

--- client/src/components/Forms/SignIn.js
+++ client/src/components/Forms/SignIn.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Forms.scss";
 import UserService from "../../services/UserService";
 import { useHistory } from "react-router-dom";
